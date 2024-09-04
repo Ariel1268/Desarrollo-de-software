05e9aa3b34243206d9adcf0ac118b913ea40a494
Ariel1268 <113410144+Ariel1268@users.noreply.github.com>
2024-09-03 19:07:18 -0500

    Add files via upload

---
+++ Universidad/src/Universidad/Persona.java
@@ -0,0 +1,22 @@
+
+package Universidad;
+
+
+public class Persona {
+  
+    private int cedula;
+    private String nombre;
+    private String fechaNacimiento;
+    private String lugarNacimiento;
+
+    public int getEdad() {
+        // Lógica para obtener la edad
+        return 0;
+    }
+
+    public String getNombreCompleto() {
+        return nombre;
+    }
+}
+    
+
---
+++ Universidad/src/Universidad/Profesor.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Universidad;
+
+/**
+ *
+ * @author alanp
+ */
+public class Profesor extends Persona {
+    private String profesion;
+    private String nacionalidad;
+    private double sueldo;
+
+    public void agregarCurso(Cursos curso) {
+        // Lógica para agregar curso
+    }
+
+    public void obtenerCurso() {
+        // Lógica para obtener curso
+    }
+
+    public void removerCurso(Cursos curso) {
+        // Lógica para remover curso
+    }
+}
